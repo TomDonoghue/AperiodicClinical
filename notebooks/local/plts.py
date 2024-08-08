d31b8a9507ff6a19884fcc6020236377809dcbfe
Tom Donoghue <tdonoghue.research@gmail.com>
2024-08-08 14:13:58 -0400

    update local code

--- notebooks/local/plts.py
+++ notebooks/local/plts.py
@@ -4,16 +4,23 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+
+from .utils import sort_df
+
 ####################################################################################################
 ####################################################################################################
 
-def plot_hist(df, column, figsize=(3, 3), **kwargs):
+def plot_hist(df, column, figsize=(3, 3), sort=True, end_label=None, **kwargs):
     """Helper function to plot a histogram of data."""
     
     # Settings
     #color = '#0d72a8'
     color = '#111dcf'
     shrink = 0.8
+    
+    if sort:
+        df = sort_df(df, column, end_label)
     
     plt.figure(figsize=figsize)
     hist = sns.histplot(df, x=column, shrink=shrink, color=color, **kwargs)
--- notebooks/local/utils.py
+++ notebooks/local/utils.py
@@ -1,6 +1,9 @@
 """Utilities for the aperiodic-clinical project."""
 
+from collections import Counter
+
 import numpy as np
+import pandas as pd
 
 from specparam.utils import trim_spectrum
 
@@ -23,9 +26,65 @@ def compute_abs_power(freqs, powers, band, method='sum'):
 
     return avg_power
 
+
 ## DATAFRAME MANAGEMENT UTILS
+
+def sort_df(df, column, end_label=None, copy=True):
+    """Sort the entries in a dataframe by count."""
+    
+    if copy:
+        df = df.copy()    
+    
+    labels = [el[0] for el in Counter(df[column]).most_common()]
+    
+    if end_label:
+        labels.remove(end_label)
+        labels = labels + [end_label]
+
+    df[column] = pd.Categorical(df[column], labels)
+    
+    return df
+
 
 def min_count(df, column, min_value):
     """Helper function for sub-selecting a minimum number of values."""
     
     return df.groupby(column).filter(lambda x : len(x) > min_value)
+
+
+def replace_multi_str(df, column, fill='multiple'):
+    """Replace entries with multiple values with a string."""
+    
+    split = '\u2028'
+    multicol = df[column].str.contains(split).values
+    for rind, row in df[multicol].iterrows():
+        df.loc[rind, column] = fill
+    
+    return df
+
+
+def replace_multi_int(df, column):
+    """Replace entries with multiple values with the sume."""
+    
+    split = '\u2028'
+    dubvals = df[column].str.contains(split).values
+    for rind, row in df[dubvals].iterrows():
+        df.loc[rind, column] = sum([int(el) for el in row[column].split(split)])
+        
+    return df
+
+
+def replace_other(df, column, min_val):
+    """Replace infrequent items with an 'other' category."""
+
+    df = df.copy()
+    
+    counts = Counter(df[column])
+    
+    kept = [label for label, count in counts.items() if count >= min_val]
+    dropped = [label for label, count in counts.items() if count < min_val]
+    
+    df = df.replace(dropped, 'other')
+    df[column] = pd.Categorical(df[column], kept + ['other'])    
+    
+    return df
